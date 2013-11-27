b59e02f9f3922390566854d8c6e3a6acc7337379
serycjon <jonas.serych@gmail.com>
2013-11-27 23:07:09 +0100

    added topology table initialization

--- src/routing_table.c
+++ src/routing_table.c
@@ -9,8 +9,13 @@ int load_config_from_file(char* filename, int local_id){
 	int nodes_count = MAX_NODES;
 	int neighbors_counts[nodes_count];
 	int** topology_table;
+	int i;
+	topology_table = (int **) malloc(MAX_NODES * sizeof(int *));
+	for(i=0; i<MAX_NODES; i++){
+		topology_table[i] = (int *) malloc(MAX_CONNECTIONS * sizeof(int));
+	}
 	int result = parseRouteConfiguration(filename, local_id, &local_port, &connection_count, connections,
-						&nodes_count, neighbors_counts, topology_table);
+			&nodes_count, neighbors_counts, topology_table);
 	if (!result) {
 		return 1;
 	}
