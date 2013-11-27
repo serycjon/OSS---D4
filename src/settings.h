dcedc5d1e6995feb14ddff5ffc1aa81f51d26a28
serycjon <jonas.serych@gmail.com>
2013-11-27 21:40:34 +0100

    all important constants should go there...

---
+++ src/settings.h
@@ -0,0 +1,11 @@
+#ifndef SETTINGS_GUARD
+#define SETTINGS_GUARD
+
+
+#define DEFAULT_CONFIG_FILENAME "routing.cfg"
+#define MIN_ID 1
+#define MAX_ID 255
+#define MAX_NODES 255
+#define MAX_CONNECTIONS 5
+
+#endif
