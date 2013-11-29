a8079dd26ffae0c23485d2bce801336e038b2e7c
Kata <klimoka@fel.cvut.cz>
2013-11-29 21:53:20 +0100

    add StackEntry structure add createRoutingTable function

--- src/routing_table.h
+++ src/routing_table.h
@@ -14,6 +14,13 @@ typedef struct routing_table_entry {
 	char next_hop_ip[IP_ADDRESS_MAX_LENGTH];
 } RoutingTableEntry;
 
+typedef struct stack_entry {
+	int final_node_ID;
+	int first_hop_node_ID;
+} StackEntry;
+
 int initRouting(char* filename, int local_id);
+
+RoutingTableEntry* createRoutingTable(TopologyTable* topology, int node_ID, NodeStatus* status_table);
 
 #endif
