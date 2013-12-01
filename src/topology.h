8ffb7a228e179c1b8bc301f6acb7d2ee4da12e06
serycjon <jonas.serych@gmail.com>
2013-12-01 16:31:47 +0100

    moved topology issues here

--- src/topology.h
+++ src/topology.h
@@ -1,11 +1,13 @@
 #ifndef TOPOLOGY_GUARD
 #define TOPOLOGY_GUARD
 
+
 typedef struct topology_table{
 	int nodes_count;
 	int* neighbors_counts;
 	int** table;
 } TopologyTable;
+
 
 int insertIntoTopologyTable(int from, int to, TopologyTable* p_topology);
 void showTopology(TopologyTable topology);
