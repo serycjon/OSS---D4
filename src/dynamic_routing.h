ac935b886d76bd3426e3ab9047c212ddaf9a8e99
serycjon <jonas.serych@gmail.com>
2013-12-04 23:16:50 +0100

    changed real_connection strucs'member addrinfo to sockaddr moved mem_and_sfd to main.h removed routingListen and sayHello added inInit and helloSender

--- src/dynamic_routing.h
+++ src/dynamic_routing.h
@@ -2,27 +2,23 @@
 #define DYNAMIC_ROUTING_GUARD
 
 #include <time.h>
+#include "main.h"
 #define OUT_CONN 1
 #define IN_CONN 0
 
 struct real_connection{
 	char type;
 	int id;
-	struct addrinfo *addrinfo; // for type == 0 (arrow in)
+	struct sockaddr *addr; // for type == 0 (arrow in)
 	int sockfd; // for type == 1 (arrow out)
 	clock_t last_seen;
 	char online; // if timed out, set to 0 => only one NSU
 };
 
-struct mem_and_sfd{
-	int sfd;
-	struct shared_mem *mem;
-};
-	
 
-int routingListen(void* port_number);
-int sayHello(Connections conns);
+int inInit(void* mem);
 void outInit(struct shared_mem *mem, Connections out_conns);
 void sockListener(void *param);
+void helloSender(void *param);
 
 #endif
