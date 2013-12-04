03c6de29102b00fe899014f68ebf0df511d8b10c
serycjon <jonas.serych@gmail.com>
2013-12-04 15:57:42 +0100

    changed structure

--- src/main.h
+++ src/main.h
@@ -6,7 +6,7 @@
 #include "dynamic_routing.h"
 
 struct shared_mem {
-	struct real_connection *p_connections[MAX_ID+1];
+	struct real_connection *p_connections;
 	TopologyTable *p_topology;
 	RoutingTable *p_routing_table;
 	NodeStatus **p_status_table;
