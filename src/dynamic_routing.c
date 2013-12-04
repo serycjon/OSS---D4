c6fe43dfe7b715e5bab0596df3dbaff9e1ba4691
serycjon <jonas.serych@gmail.com>
2013-12-04 15:09:05 +0100

    added outInit() and sockListener()

--- src/dynamic_routing.c
+++ src/dynamic_routing.c
@@ -7,8 +7,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <pthread.h>
 
 #include "route_cfg_parser.h"
+#include "dynamic_routing.h"
+#include "main.h"
 
 #define MAXBUFLEN 100
 
@@ -152,3 +155,126 @@ int sayHello(Connections conns)
 	}
 	return 0;
 }
+
+void outInit(struct shared_mem *mem, Connections out_conns)
+{
+	int i;
+	struct addrinfo hints;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;          /* Any protocol */
+
+
+	for(i=0; i<out_conns.count; i++){
+		struct addrinfo *result, *rp;
+		int sfd, s;
+		char port_str[5];
+		sprintf(port_str, "%d", out_conns.array[i].port);
+		/* Obtain address(es) matching host/port */
+		printf("trying to connect to %s:%s\n", out_conns.array[i].ip_address, port_str);
+		s = getaddrinfo(out_conns.array[i].ip_address, port_str, &hints, &result);
+		if (s != 0) {
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+			//exit(EXIT_FAILURE);
+			continue;
+		}
+
+		/* getaddrinfo() returns a list of address structures.
+		   Try each address until we successfully connect(2).
+		   If socket(2) (or connect(2)) fails, we (close the socket
+		   and) try the next address. */
+
+		for (rp = result; rp != NULL; rp = rp->ai_next) {
+			sfd = socket(rp->ai_family, rp->ai_socktype,
+					rp->ai_protocol);
+			if (sfd == -1){
+				perror("outInitSocket");
+				continue;
+			}
+
+			if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1){
+				// printf("connected!!!\n");
+				break;                  /* Success */
+			}
+			perror("outInitConnect");
+
+			close(sfd);
+		}
+
+		if (rp == NULL) {               /* No address succeeded */
+			fprintf(stderr, "Could not connect!!!\n");
+			//exit(EXIT_FAILURE);
+			continue;
+		}
+
+		freeaddrinfo(result);           /* No longer needed */
+
+		/* Send remaining command-line arguments as separate
+		   datagrams, and read responses from server */
+		//printf("created socket: %d\n", sfd);
+
+		struct mem_and_sfd *param;
+		param = (struct mem_and_sfd*) malloc(sizeof(struct mem_and_sfd));
+		param->sfd = sfd;
+		param->mem = mem;
+
+		pthread_t listen_thread;
+		pthread_create(&listen_thread, NULL, (void*) &sockListener, (void*) param);
+
+		sendto(sfd, "Ahojky!", strlen("Ahojky!")+1, 0, 0, 0);
+		/*
+		   struct real_connection *rc = mem->p_connections[out_conns.array[i].id];
+		   rc->type = OUT_CONN; // OUT connection
+		   rc->id = out_conns.array[i].id;
+		   rc->last_seen = clock();
+		   rc->online = 0;
+		   */
+
+
+
+	}
+}
+
+void sockListener(void *param)
+{
+	printf("starting new listener...\n");
+	int sfd = ((struct mem_and_sfd*)param)->sfd;
+	//printf("using listening socket: %d\n", sfd);
+	int s;
+	struct sockaddr_storage peer_addr;
+	socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
+	ssize_t nread;
+	char buf[BUF_SIZE];
+
+	for (;;) {
+		nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+				(struct sockaddr *) &peer_addr, &peer_addr_len);
+		if (nread == -1)
+			continue;               /* Ignore failed request */
+
+		char host[NI_MAXHOST], service[NI_MAXSERV];
+
+		s = getnameinfo((struct sockaddr *) &peer_addr,
+				peer_addr_len, host, NI_MAXHOST,
+				service, NI_MAXSERV, NI_NUMERICSERV);
+		if (s == 0){
+			printf("Received %ld bytes from %s:%s\n",
+					(long) nread, host, service);
+			printf("got: %s\n", buf);
+		}else{
+			fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
+		}
+
+		/*
+		 * if (sendto(sfd, buf, nread, 0,
+		 (struct sockaddr *) &peer_addr,
+		 peer_addr_len) != nread)
+		 fprintf(stderr, "Error sending response\n");
+		 */
+		//sleep(5);
+	}
+}
+
