512c7bbfe4d42c6c7aa66543f29b5e630433cc50
serycjon <jonas.serych@gmail.com>
2013-12-04 15:04:55 +0100

    init commit... (shared_mem definition)

---
+++ src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_GUARD
+#define MAIN_GUARD
+#include "settings.h"
+#include "topology.h"
+#include "routing_table.h"
+#include "dynamic_routing.h"
+
+struct shared_mem {
+	struct real_connection *p_connections[MAX_ID+1];
+	TopologyTable *p_topology;
+	RoutingTable *p_routing_table;
+	NodeStatus **p_status_table;
+};
+
+#endif
