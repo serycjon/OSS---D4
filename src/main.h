736128bca1fad6356bb3da57a35e74677e4d83cd
serycjon <jonas.serych@gmail.com>
2013-12-04 23:12:17 +0100

    moved parameter passing structs here

--- src/main.h
+++ src/main.h
@@ -6,10 +6,24 @@
 #include "dynamic_routing.h"
 
 struct shared_mem {
+	int local_id;
+	int local_port;
 	struct real_connection *p_connections;
 	TopologyTable *p_topology;
 	RoutingTable *p_routing_table;
 	NodeStatus **p_status_table;
 };
+
+struct mem_and_sfd{
+	int sfd;
+	struct shared_mem *mem;
+};
+
+struct mem_and_buffer{
+	char *buf;
+	int len;
+	struct shared_mem *mem;
+};
+	
 
 #endif
