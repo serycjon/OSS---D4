ef058b97b6231c4597abf01c05b8be10c73a5a10
serycjon <jonas.serych@gmail.com>
2013-12-05 00:06:13 +0100

    added parse functions, proper formHelloPacket

--- src/packets.c
+++ src/packets.c
@@ -1,18 +1,74 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "packets.h"
 #include "main.h"
 
-char *formHelloPacket(int id)
+char *formHelloPacket(int id, int *len)
 {
-	char *msg = (char *) malloc(20*sizeof(char));
-	sprintf(msg, "Hi from %d!", id);
+	char *msg = (char *) malloc(2*sizeof(char));
+	msg[0] = T_HELLO;
+	msg[1] = (u_char) id;
+	// sprintf(msg+1, "Hi from %d!", id);
+	*len = 2; //strlen(msg+1)+sizeof(char);
 	return msg;
 }
 
 void packetParser(void *parameter)
 {
 	struct mem_and_buffer *params = (struct mem_and_buffer *) parameter;
-	printf("received: %s\n", params->buf);
+	char *buf = params->buf;
+	//printf("received: %s\n", buf);
 
+	char type = buf[0];
+	switch(type){
+		case T_MSG:
+			printf("MESG!\n");
+			break;
+		case T_HELLO:
+			parseHello(params);
+			break;
+		case T_NSU:
+			printf("NSU!\n");
+			break;
+		case T_DD:
+			printf("DD!\n");
+			break;
+		default:
+			printf("INVALID TYPE!!!\n");
+	}
+
+
+}
+
+void parseMsg(struct mem_and_buffer *params)
+{
+	char *buf = params->buf;
+
+	int dest_id = (int)buf[1];
+	int source_id = (int)buf[2];
+	printf("Received MSG from %d to %d:\n"
+			"%s\n", source_id, dest_id, buf+3);
+}
+
+void parseHello(struct mem_and_buffer *params)
+{
+	int len = params->len;
+	char *buf = params->buf;
+
+	if(len!=2*sizeof(char)){
+		printf("INVALID hello!\n");
+	}
+	int source_id = (int)buf[1];
+	printf("HELLO from %d!\n", source_id);
+}
+
+void parseNSU(struct mem_and_buffer *params)
+{
+	printf("received NSU\n");
+}
+
+void parseDD(struct mem_and_buffer *params)
+{
+	printf("received DD\n");
 }
--- src/packets.h
+++ src/packets.h
@@ -1,7 +1,18 @@
 #ifndef PACKETS_GUARD
 #define PACKETS_GUARD
 
-char *formHelloPacket(int id);
+#include "main.h"
+#define T_MSG 0
+#define T_HELLO 1
+#define T_NSU 2
+#define T_DD 3
+
+char *formHelloPacket(int id, int *len);
 void packetParser(void *parameter);
+
+void parseMsg(struct mem_and_buffer *params);
+void parseHello(struct mem_and_buffer *params);
+void parseNSU(struct mem_and_buffer *params);
+void parseDD(struct mem_and_buffer *params);
 
 #endif
