c6bfcdca1d618b8c833cb88dfde401714ff68663
serycjon <jonas.serych@gmail.com>
2013-12-06 00:40:56 +0100

    changed param struct name

--- src/packets.h
+++ src/packets.h
@@ -11,9 +11,9 @@ char *formHelloPacket(int id, int *len);
 char *formMsgPacket(int source_id, int dest_id, char* text, int *len);
 void packetParser(void *parameter);
 
-void parseMsg(struct mem_and_buffer *params);
-void parseHello(struct mem_and_buffer *params);
-void parseNSU(struct mem_and_buffer *params);
-void parseDD(struct mem_and_buffer *params);
+void parseMsg(struct mem_and_buffer_and_sfd *params);
+void parseHello(struct mem_and_buffer_and_sfd *params);
+void parseNSU(struct mem_and_buffer_and_sfd *params);
+void parseDD(struct mem_and_buffer_and_sfd *params);
 
 #endif
