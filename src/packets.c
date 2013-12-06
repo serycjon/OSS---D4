f67f13b468a8ff03a44ed40a565332b2a6476e92
serycjon <jonas.serych@gmail.com>
2013-12-06 13:01:47 +0100

    added formNSUPacket() parseNSU now working... corrected packetParser

--- src/packets.c
+++ src/packets.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "dynamic_routing.h"
 #include "packets.h"
 #include "main.h"
+
+char *formNSUPacket(int id, int new_state, int *len)
+{
+	char *msg = (char *) malloc(3*sizeof(char));
+	msg[0] = T_NSU;
+	msg[1] = (char) id;
+	msg[2] = (char) new_state;
+	// sprintf(msg+1, "Hi from %d!", id);
+	*len = 3; //strlen(msg+1)+sizeof(char);
+	return msg;
+}
 
 char *formHelloPacket(int id, int *len)
 {
@@ -35,16 +47,16 @@ void packetParser(void *parameter)
 	char type = buf[0];
 	switch(type){
 		case T_MSG:
-			printf("MESG!\n");
+			parseMsg(params);
 			break;
 		case T_HELLO:
 			parseHello(params);
 			break;
 		case T_NSU:
-			printf("NSU!\n");
+			parseNSU(params);
 			break;
 		case T_DD:
-			printf("DD!\n");
+			parseDD(params);
 			break;
 		default:
 			printf("INVALID TYPE!!!\n");
@@ -71,22 +83,35 @@ void parseHello(struct mem_and_buffer_and_sfd *params)
 	}
 	int source_id = (int)buf[1];
 	struct real_connection *conn = &(params->mem->p_connections[source_id]);
-	if(conn->online==0){
-		printf("NODE %d WENT ONLINE!\n", source_id);
+	if(conn->online==OFFLINE){
+		reactToStateChange(source_id, ONLINE, params->mem);
 	}
 	conn->type = IN_CONN;
 	conn->id = source_id;
 	conn->addr = params->addr;
 	conn->sockfd = params->sfd;
 	conn->last_seen = time(NULL);
-	conn->online = 1;
+	conn->online = ONLINE;
 
 	//printf("HELLO from %d!\n", source_id);
 }
 
 void parseNSU(struct mem_and_buffer_and_sfd *params)
 {
-	printf("received NSU\n");
+	//printf("received NSU\n");
+	int len = params->len;
+	char *buf = params->buf;
+
+	if(len!=3*sizeof(char)){
+		printf("INVALID NSU!\n");
+	}
+	int id = (int)buf[1];
+	int new_state = (int)buf[2];
+	
+	if(params->mem->p_status_table[id]!=new_state){
+		printf("I have heard that node %d has changed its state!\n", id);
+		reactToStateChange(id, new_state, params->mem);
+	}
 }
 
 void parseDD(struct mem_and_buffer_and_sfd *params)
