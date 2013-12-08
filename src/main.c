007410cf7f5d4179586ef9c7169ad6b883551709
Kata <klimoka@fel.cvut.cz>
2013-12-08 14:42:45 +0100

    add mutex initialization + pthread.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "main.h"
 #include "routing_table.h"
 #include "settings.h"
@@ -39,9 +40,10 @@ int main(int argc, char** argv)
 
 	struct shared_mem *p_mem = (struct shared_mem *) malloc(sizeof(struct shared_mem));
 	// Process the config file
-	// pthread_mutex_init(&p_mem.mutexes.routing_mutex, NULL);
-	// pthread_mutex_init(&p_mem.mutexes.status_mutex, NULL);
-	// pthread_mutex_init(&p_mem.mutexes.connection_mutex, NULL);
+	p_mem->mutexes = (struct all_mutexes *) malloc(sizeof(struct all_mutexes));
+	pthread_mutex_init(&p_mem->mutexes->routing_mutex, NULL);
+	pthread_mutex_init(&p_mem->mutexes->status_mutex, NULL);
+	pthread_mutex_init(&p_mem->mutexes->connection_mutex, NULL);
 	initRouting(config_file_name, node_id, p_mem);
 	printf("Topology table size: %d\n", p_mem->p_topology->nodes_count);
 	showRoutingTable(p_mem);
