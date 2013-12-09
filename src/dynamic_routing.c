bcddb0045bcfe87c5e082d282a7a581c2c62fc5c
serycjon <jonas.serych@gmail.com>
2013-12-09 02:20:54 +0100

    sendToId now returning -1 on failure supressed output (#DEBUG)

--- src/dynamic_routing.c
+++ src/dynamic_routing.c
@@ -395,25 +395,30 @@ void sendToNeighbour(int dest_id, char *packet, int len, struct shared_mem *p_me
 	pthread_mutex_unlock(&(p_mem->mutexes->connection_mutex));
 }
 
-void sendToId(int dest_id, char *packet, int len, struct shared_mem *p_mem, int retry)
+int sendToId(int dest_id, char *packet, int len, struct shared_mem *p_mem, int retry)
 {
 	pthread_mutex_lock(&(p_mem->mutexes->routing_mutex));
 	if(dest_id > p_mem->p_routing_table->size){
 		printf("cannot reach node %d -- ID too big!\n", dest_id);
 		pthread_mutex_unlock(&(p_mem->mutexes->routing_mutex));
-		return;
+		return -1;
 	}else{
 		pthread_mutex_unlock(&(p_mem->mutexes->routing_mutex));
 	}
 	if(dest_id == p_mem->local_id){
 		printf("why would you send anything to yourself!?!\n");
-		return;
+		return -1;
 	}
 	pthread_mutex_lock(&(p_mem->mutexes->routing_mutex));
+#ifdef DEBUG
+	printf("trying to reach id %d\n", dest_id);
+#endif
 	int next_id = p_mem->p_routing_table->table[idToIndex(dest_id)].next_hop_id;
 	if(next_id==-1){
+#ifdef DEBUG
 		printf("TODO: diff between totally unreachable and tmporarily unreachable node!\n");
-		printf("cannot reach node %d\n", dest_id);
+#endif
+		printf("cannot reach node %d. %s\n", dest_id, retry==RETRY?"Will try it later":"");
 		pthread_mutex_unlock(&(p_mem->mutexes->routing_mutex));
 
 		if(retry==RETRY){
@@ -421,9 +426,10 @@ void sendToId(int dest_id, char *packet, int len, struct shared_mem *p_mem, int 
 			addWaitingMessage(dest_id, len, packet, p_mem);;
 			pthread_mutex_unlock(&(p_mem->mutexes->buffer_mutex));
 		}
-		return;
+		return -1;
 	}else{
 		pthread_mutex_unlock(&(p_mem->mutexes->routing_mutex));
 	}
 	sendToNeighbour(next_id, packet, len, p_mem);
+	return 0;
 }
