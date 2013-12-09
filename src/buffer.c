02ff34d85da0866c11506b70ab4d75e3ca81b2dc
serycjon <jonas.serych@gmail.com>
2013-12-09 19:59:27 +0100

    added lock to showUndeliveredMessage() addWaitingMessage calloc size corrected (?)

--- src/buffer.c
+++ src/buffer.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
@@ -19,6 +20,7 @@ void showUndeliveredMessage(UndeliveredMessage *ptr)
 
 void showUndeliveredMessages(struct shared_mem *mem)
 {
+	pthread_mutex_lock(&(mem->mutexes->buffer_mutex));
 	printf("-----------\n");
 	printf("Undelivered messages buffer:\n");
 	printf("-----------\n");
@@ -29,23 +31,25 @@ void showUndeliveredMessages(struct shared_mem *mem)
 		ptr = ptr->next;
 	}
 	printf("-----------\n");
+	pthread_mutex_unlock(&(mem->mutexes->buffer_mutex));
 }
 
 void resendUndeliveredMessages(int to_id, struct shared_mem *mem)
 {
 	UndeliveredMessage **ptr = &(mem->buffer);
 
-
 	while(*ptr != NULL){
 		if((*ptr)->dest_id == to_id || to_id==ALL){
 			//send
-			sendToId((*ptr)->dest_id, (*ptr)->message, (*ptr)->len, mem, NORETRY);
+			if(sendToId((*ptr)->dest_id, (*ptr)->message, (*ptr)->len, mem, NORETRY) == -1){
+				continue;
+			}
 
 			//delete from buffer
-#ifdef DEBUG
+// #ifdef DEBUG
 			printf("will delete this msg(sent): ");
 			showUndeliveredMessage(*ptr);
-#endif
+// #endif
 			UndeliveredMessage *old = ((*ptr));
 			*ptr = (*ptr)->next;
 			free(old->message);
@@ -76,7 +80,7 @@ void addWaitingMessage(int dest_id, int len, char* msg, struct shared_mem *mem)
 	UndeliveredMessage *new = (UndeliveredMessage *)malloc(sizeof(UndeliveredMessage));
 	new->dest_id = dest_id;
 	new->len = len;
-	new->message = (char *)calloc((len+1), sizeof(char));
+	new->message = (char *)calloc(len, sizeof(char));
 	memcpy(new->message, msg, len*sizeof(char));
 	new->next = NULL;
 
