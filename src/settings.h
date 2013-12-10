5c0e1531c14b6ac2276992302ef1af10bf27b093
serycjon <jonas.serych@gmail.com>
2013-12-10 01:40:13 +0100

    big network config

--- src/settings.h
+++ src/settings.h
@@ -3,19 +3,19 @@
 
 #define FAILURE 0
 #define SUCCESS 1
-#define DEFAULT_CONFIG_FILENAME "routing.cfg"
+#define DEFAULT_CONFIG_FILENAME "routing_big.cfg"
 #define MIN_ID 1
 #define MAX_ID 255
 #define MAX_NODES (MAX_ID - MIN_ID)
 #define MAX_LINKS 1000
-#define MAX_CONNECTIONS 5
+#define MAX_CONNECTIONS 8
 #define HELLO_TIMER 1
 #define DEATH_TIMER 3
 
 #define BUF_SIZE 200
 #define RESEND_BUFFER_SIZE 5
 
-#define DEBUG 1
+//#define DEBUG 1
 
 #define IP_ADDRESS_MAX_LENGTH 16
 #endif
